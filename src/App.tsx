61fdded8944b1c321d785d2bebc0eef2c7722e06
alejandrosju_meli <alejandrosebastian.juarez@mercadolibre.com>
2025-06-26 19:54:59 -0300

    add typescrit to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { JSX } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+
+// This file is required by the TypeScript compiler.
+// If you don't want to use TypeScript, you can delete this file.
+// When using the typescript version of create-react-app, this file is created automatically.
